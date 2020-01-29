245107d3b03cb7455bfe627b72b1cdec93953d78
Jasan Hernandez <jasan_azael@hotmail.es>
2020-01-29 03:08:05 -0600

    simulating Lion look

--- tiggers.py
+++ tiggers.py
@@ -1,61 +1,52 @@
 # -*- encoding: utf-8 -*-
-import pygame, sys, random
+import pygame, sys
+from Lion import Lion
 
 WHITE = (255, 255, 255)
 GREEN = (48, 163, 71)
+RED_LOCATION = (204, 78, 78);
 width = 900 
 heigth = 600
+animal_size = 50
 CONTAINER = (width, heigth)
-animal_lenght = (70, 70)
+FRAME_PIXELS = 20
 
-def run_game(window, lion, impala, 
-                     lion1, lion2, 
-                     imp1, imp2, imp3, imp4, imp5): 
+def run_game(window, LionImg, lion1, lion2, lion3, lion4): 
     while True:
         window.fill(GREEN)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        window.blit(lion, lion1)
-        window.blit(lion, lion2)
-        window.blit(impala, imp1)
-        window.blit(impala, imp2)
-        window.blit(impala, imp3)
-        window.blit(impala, imp4)
-        window.blit(impala, imp5)
+        window.blit(LionImg, lion1.lion)
+        window.blit(LionImg, lion2.lion)
+        window.blit(LionImg, lion3.lion)
+        window.blit(LionImg, lion4.lion)
+        # pygame.draw.rect(window, RED_LOCATION, lion1.paintLook)
+        # pygame.draw.rect(window, RED_LOCATION, lion2.paintLook)
+        lion1.pintarCuadros()
+        lion2.pintarCuadros()
+        lion3.pintarCuadros()
+        lion4.pintarCuadros()
+        
         pygame.display.flip()
     pygame.quit()         
-
-def draw_animals(animal):
-    animalrect = animal.get_rect()
-    animalrect.x = get_position(width - 70)
-    animalrect.y = get_position(heigth - 70)
-    
-    return animalrect
 
 def run():
     pygame.init()
     window = pygame.display.set_mode(CONTAINER)
     pygame.display.set_caption("Lions and impalas")
+    
     # images animals and longitud
-    lion = pygame.image.load("./resourses/lion.png")
-    lion = pygame.transform.scale(lion, animal_lenght)
-    impala = pygame.image.load("./resourses/impala.png")
-    impala = pygame.transform.scale(impala, animal_lenght)
-
-    lion1 = draw_animals(lion)
-    lion2 = draw_animals(lion)
-    imp1 = draw_animals(impala)
-    imp2 = draw_animals(impala)
-    imp3 = draw_animals(impala)
-    imp4 = draw_animals(impala)
-    imp5 = draw_animals(impala)
-   
-    run_game(window, lion, impala, lion1, lion2, imp1, imp2, imp3, imp4, imp5) 
-
-def get_position(lenght):
-    position = random.sample(range(1, lenght), 5)
-    return position[0]
+    lion1 = Lion(window)
+    lion2 = Lion(window)
+    lion3 = Lion(window)
+    lion4 = Lion(window)
+    
+    # print(lion1.pintarCuadros())
+    # print(lion2.pintarCuadros())
+    # print('Lion positon 0 {} position 1 {}'.format(lion.widthContainer, lion.heigthContainer))
+    
+    run_game(window, lion1.Image, lion1, lion2, lion3, lion4)
 
 if __name__ == '__main__':
     run()
